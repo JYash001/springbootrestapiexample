28d2b724adc5398c8b1026385b91d60ce2001d01
Praveen <praveenjajam@gmail.com>
2019-05-02 19:28:48 -0700

    updated

--- src/main/java/com/springboot/springbootrestapiexample/repository/EmployeeRepository.java
+++ src/main/java/com/springboot/springbootrestapiexample/repository/EmployeeRepository.java
@@ -1,7 +1,8 @@
 package com.springboot.springbootrestapiexample.repository;
 
-import com.springboot.springbootrestapiexample.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.springbootrestapiexample.model.Employee;
 
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>
